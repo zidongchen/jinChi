2d5a6fbfe7644c97eee951c58ebee9d9b6406cc0
zidongchen <zidong.chen@outlook.com>
2018-12-13 14:47:57 +0800

    v1.15 2018年12月13日
    编号、批号的问题修改

--- files/page2/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p],q,_(r,s,t,u,v,w,x,_(),y,_(z,A,B,C,D,_(E,F,G,H),I,null,J,C,K,C,L,M,N,null,O,P,Q,R,S,T,U,P),V,_(),W,_(),X,_(Y,[])),Z,_(),ba,_());}; 
-var b="url",c="page2.html",d="generationDate",e=new Date(1542793067728.34),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="NewVariable",o="heGeShuLiang",p="buHeGeShuLiang",q="page",r="packageId",s="3130eb9acd3742a2b2e0c44228a29505",t="type",u="Axure:Page",v="name",w="page2",x="notes",y="style",z="baseStyle",A="627587b6038d43cca051c114ac41ad32",B="pageAlignment",C="near",D="fill",E="fillType",F="solid",G="color",H=0xFFFFFFFF,I="image",J="imageHorizontalAlignment",K="imageVerticalAlignment",L="imageRepeat",M="auto",N="favicon",O="sketchFactor",P="0",Q="colorStyle",R="appliedColor",S="fontName",T="Applied Font",U="borderWidth",V="adaptiveStyles",W="interactionMap",X="diagram",Y="objects",Z="masters",ba="objectPaths";
-return _creator();
-})());
